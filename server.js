af85648f0f11ee94539e125d4864e08d0c30e5c4
amar-nath1 <amarat279955>
2023-03-09 16:02:19 +0530

    express form

--- server.js
+++ server.js
@@ -3,17 +3,26 @@
 const http=require('http')
 
 const express=require('express')
-
+const bodyParser=require('body-parser')
 const app=express()
 
-app.use((req,res,next)=>{
-  console.log('middleware')
-  next()
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
+
+  res.send("<form action='/product' method='POST'><input type='text' placeholder='item name' name='title'/><input type='text' placeholder='size' name='size'/> <button type='submit'>Submit</button> </form>")
+
 })
 
-app.use((req,res,next)=>{
-  
-  res.send({key1:1})
+app.use('/product',(req,res,next)=>{
+  console.log(req.body)
+  res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
+
+  res.send("<h2>mil gya item. or aa gya home par</h2>")
+
 })
 
 const server=http.createServer(app)
